b6630fa5a321649637b176ea6f845f3a5e7ad392
Lior Yehezkely <liorye03@gmail.com>
2020-07-21 17:55:00 +0300

    Add solution for problem `B - Sequential Nim` Link: https://codeforces.com/contest/1382/problem/B Tags: dp, games, *1100 Ref: https://codeforces.com/contest/1382/submission/87541591

---
+++ codeforces/1382/B.cpp
@@ -0,0 +1,78 @@
+//https://codeforces.com/contest/1382/problem/B
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+
+
+const int maxn = 1e5 + 5;
+
+int a[maxn] = { 0 };
+
+void solve();
+int main()
+{
+    GOGOGO
+    int t; cin >> t; while(t--) solve();
+    BYEBYE
+}
+
+void solve()
+{
+
+    int n;
+    cin >> n;
+    rep(i, n) {cin >> a[i];}
+    bool losing = false;
+    for(int i = n-2; i >= 0; --i)
+    {
+        if (a[i] != 1) 
+        {
+            losing = false;
+        }
+        else
+        {
+            losing = !losing;
+        }
+        
+
+        
+    }
+    cout << ((losing) ? "Second" : "First") << endl;
+}
