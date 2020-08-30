305300634ec3b55f03bc357f88dab240d83a1896
Lior Yehezkely <liorye03@gmail.com>
2020-08-30 17:40:00 +0300

    Add solution for problem `A - Juggling Letters` Link: https://codeforces.com/contest/1397/problem/A Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1397/submission/91355440

---
+++ codeforces/1397/A.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int cnt[26] = { 0 };
+
+void solve()
+{
+    int n; cin >> n;
+    clrcin
+    vector<string> s(n);
+    rep(i, 26)
+    {
+        cnt[i] = 0;
+    }
+    rep(i, n)
+    {
+        cin >> s[i];
+        for(auto e : s[i])
+        {
+            cnt[e-'a']++;
+        }
+    }
+    for(int i = 0; i < 26; ++i)
+    {
+        if(cnt[i] % n)
+        {
+            cout << "NO" << endl; return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    int t;
+    //t=1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
