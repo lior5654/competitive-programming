f3f80a3b2992671dd0aab56787ab74dc00793ebc
Lior Yehezkely <liorye03@gmail.com>
2020-09-06 16:51:00 +0300

    Add solution for problem `D - Mishka and Interesting sum` Link: https://codeforces.com/contest/703/problem/D Tags: data structures, *2100 Ref: https://codeforces.com/contest/703/submission/92019923

--- codeforces/703/D.cpp
+++ codeforces/703/D.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp> // Common file
 #include <ext/pb_ds/tree_policy.hpp> // Including 
@@ -50,11 +49,11 @@ const int INFI = 1e9 + 5;
 const ll INFL = 4e18 + 5;
  
 // LIORZ LIORZ LIORZ LIORZ LIORZ
- 
+
 int n; const int maxn = 1e6 + 5;
 vpi g[maxn]; ll a[maxn] = { 0 }; ll fen[maxn] = { 0 }; ll xpre[maxn] = { 0 }; ll ans[maxn] = { 0 };
 unordered_map<ll, ll> last; ll everything = 0;
- 
+
 void _update(int pos, ll val) // fen[pos] ^= val
 {
     pos++;
@@ -97,7 +96,7 @@ void solve()
     rep(i, m)
         cout << ans[i] << '\n';
 }
- 
+
 signed main()
 {
     GOGOGO
