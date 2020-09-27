2ba8c9546e19c480ea78899aa90a46e899135bab
Lior Yehezkely <liorye03@gmail.com>
2020-09-27 12:42:00 +0300

    Add solution for problem `E - Excitation of Atoms` Link: https://codeforces.com/contest/1425/problem/E Tags: greedy, implementation, *2200 Ref: https://codeforces.com/contest/1425/submission/93943333

--- codeforces/1425/E.cpp
+++ codeforces/1425/E.cpp
@@ -49,21 +49,15 @@ void solveEven() {
 	cout << bst << endl;
 }
 
-void solveOne() {
+ll solveOne() {
 	ll bst = max(0LL, bestSufTake[1]);
 	for (int i = 1; i < n - 1; i++) bst = max(bst, pre[i] - mnpre[i] + bestSufTake[i + 1]);
 	for (int i = 1; i < n - 1; i++) bst = max(bst, max(bestSufTakeFromPre[i - 1] - add[i], bestSufTake[i + 1]) + max(0LL, add[i] - cost[i]));
-	cout << bst << endl;
+	return bst;
 }
 
 void solveOdd() {
 	ll bst = max(0LL, bestSufTake[1]);
-    ll best = add[0] - cost[0];
-    /*for(int i = 1; i < n - 1; ++i)
-    {
-        bst = max(best + bestSufTake[i+1], bst);
-        best = max(best + add[i], add[i] - cost[i]);
-    }*/
 	ford(i, n - 2, 0) {
 		if (i == 1)continue;
 		bst = max(bst, pre[n - 1] - cost[i]);
@@ -71,6 +65,7 @@ void solveOdd() {
 	if (n == 4) {
 		bst = max(bst, pre[n - 1] - cost[1]);
 		bst = max(bst, pre[n - 1] - cost[0]);
+		cout << max(bst,solveOne()) << endl;
 	}
 	else {
 		bst = max(bst, pre[n - 1] - add[n - 1] - cost[1]);
@@ -78,8 +73,8 @@ void solveOdd() {
 			if (i == 1 || i == 2)continue;
 			bst = max(bst, pre[n - 1] - add[i] - cost[1]);
 		}
+		cout << max(bst,solveOne()) << endl;
 	}
-    cout << bst << endl;
 }
 
 int main() {
@@ -87,7 +82,7 @@ int main() {
 	input();
 	initPreSuf();
 	if (k == 0) solveZero();
-	else if (k == 1) solveOne();
+	else if (k == 1) cout << solveOne() << endl;
 	else if (k % 2 == 0) solveEven();
 	else solveOdd();
 	return 0;
