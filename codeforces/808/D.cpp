6824c8795929700357218c61b83cadd19ce136e6
Lior Yehezkely <liorye03@gmail.com>
2020-09-27 22:43:00 +0300

    Add solution for problem `D - Array Division` Link: https://codeforces.com/contest/808/problem/D Tags: binary search, data structures, implementation, *1900 Ref: https://codeforces.com/contest/808/submission/94030113

---
+++ codeforces/808/D.cpp
@@ -0,0 +1,300 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+template<class T> using func = function<T>;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int maxn = 1e5 + 5;
+int n;
+ll a[maxn];
+ll pre[maxn];
+ll suf[maxn];
+//#define BRUH_WHY_TESTCASES
+void solve()
+{
+    cin >> n;
+    aread(a, n);
+    pre[0] = a[0];
+    for(int i = 1; i < n; ++i)
+    {
+        pre[i] = a[i] + pre[i-1];
+    }
+    suf[n-1] = a[n-1];
+    for(int i = n - 2; i >= 0; --i)
+    {
+        suf[i] = a[i] + suf[i+1];
+    }
+    if(suf[0]%2)
+    {
+        cout << "NO" << '\n'; return;
+    }
+    ll sm = suf[0];
+    sl seen;
+    seen.insert(0);
+    seen.insert(pre[0]);
+    for(int i = 1; i < n; ++i)
+    {
+        if(seen.count(sm/2 - a[i]))
+        {
+            cout << "YES" << '\n';  return;
+        }
+        seen.insert(pre[i]);
+    }
+    seen.clear();
+    seen.insert(suf[n-1]);
+    seen.insert(0);
+    for(int i = n-2; i >= 0; --i)
+    {
+        if(seen.count(sm/2 - a[i]))
+        {
+            cout << "YES" << '\n';  return;
+        }
+        seen.insert(suf[i]);
+    }
+    
+    /*si seen;
+    seen.insert(a[0]);
+    for(int i = 1; i < n - 1; ++i)
+    {
+        seen.insert(a[i]);
+        if(!((pre[i] - suf[i+1]) % 2) && seen.count((pre[i]-suf[i])/2))
+        {
+            cout << "YES" << '\n';  return;
+        }
+
+    }
+    seen.clear();
+    seen.insert(a[n-1]);
+    for(int i = n-2; i >= 1; --i)
+    {
+        seen.insert(a[i]);
+        if(!((suf[i] - pre[i-1]) % 2) && seen.count((suf[i] - pre[i-1])/2))
+        {
+            cout << "YES" << '\n';  return;
+        }
+
+    }
+    for(int i = 0; i < n; ++i)
+    {
+        if(pre[i] == suf[i+1])
+        {
+             cout << "YES" << '\n';  return;
+        }
+    }*/
+    cout << "NO" << '\n';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+        cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
