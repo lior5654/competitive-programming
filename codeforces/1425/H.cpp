a9a9dafa015cbff4750b241f77f0ae742c1a7523
Lior Yehezkely <liorye03@gmail.com>
2020-09-27 08:24:00 +0300

    Add solution for problem `H - Huge Boxes of Animal Toys` Link: https://codeforces.com/contest/1425/problem/H Tags: constructive algorithms, *1300 Ref: https://codeforces.com/contest/1425/submission/93920967

--- codeforces/1425/H.cpp
+++ codeforces/1425/H.cpp
@@ -31,7 +31,7 @@
 -hrr-    )-.__,-- ||___,--' `-.
         /._______,|__________,'\
         `--.____,'|_________,-'
- 
+
                              __
                    _ ,___,-'",-=-.
        __,-- _ _,-'_)_  (""`'-._\ `.
@@ -57,17 +57,17 @@
           /             |               \
        ,-'              |               /
       /                 |             -'
- 
+
 */
- 
+
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp> // Common file
 #include <ext/pb_ds/tree_policy.hpp> // Including 
- 
+
 using namespace __gnu_pbds;
 using namespace std;
- 
- 
+
+
 typedef long long int ll;
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pl;
@@ -90,29 +90,29 @@ less<int>,
 rb_tree_tag,
 tree_order_statistics_node_update>
 ordered_set;
- 
- 
+
+
 typedef long double ld;
 #define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
 #define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
 #define BYEBYE return 0;
- 
+
 #define all(cn) (cn).begin(), (cn).end()
 #define rep(i, n) for (int i = 0; i < n; ++i)
 #define repk(i, k, n) for(int i = k; i < n; ++i)
- 
+
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
- 
+
 #define popcnt __builtin_popcount
 #define gcd std::__detail::__gcd
 #define lcm std::__detail::__lcm
- 
+
 const int INFI = 1e9 + 5;
 const ll INFL = 4e18 + 5;
- 
+
 template<class T> void aread(T* arr, int ___n)
 {
     rep(i, ___n)
@@ -120,32 +120,32 @@ template<class T> void aread(T* arr, int ___n)
         cin >> arr[i];
     }
 }
- 
- 
+
+
 // LIORZ LIORZ LIORZ LIORZ LIORZ
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #define BRUH_WHY_TESTCASES
 #define YA "Ya"
 #define NO "Tidak"
 const int maxn = 1e5 + 5;
- 
- 
+
+
 void solve()
 {
     ll a, b, c, d; cin >> a >> b >> c >> d;
@@ -160,36 +160,36 @@ void solve()
     cout << (!parity && s ? YA : NO) << ' ';
     cout << (!parity && B ? YA : NO) << ' ';
     cout << endl;
- 
+
 }
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 signed main()
 {
     GOGOGO
@@ -204,7 +204,7 @@ signed main()
     }
     BYEBYE
 }
- 
+
 /* PLEASE READ THIS
 * N = 1
 * GUESS A!!!!!!!
