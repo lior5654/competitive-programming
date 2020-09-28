28b245c43cd95e13ea0c4737b5a4c80e6192fecb
Lior Yehezkely <liorye03@gmail.com>
2020-09-28 14:23:00 +0300

    Add solution for problem `B - ZgukistringZ` Link: https://codeforces.com/contest/551/problem/B Tags: brute force, constructive algorithms, implementation, strings, *1800 Ref: https://codeforces.com/contest/551/submission/94119471

---
+++ codeforces/551/B.cpp
@@ -0,0 +1,283 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+template<class T> using func = function<T>;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int maxn = 0;
+
+//#define BRUH_WHY_TESTCASES
+void solve()
+{
+    array<string, 3> in;
+    rep(i, 3)
+    {
+        cin >> in[i];
+    }
+    array<array<int, 26>, 3> cnt = { array<int, 26>({ 0 }) };
+    rep(i, 3)
+    {
+        for(auto c : in[i])
+        {
+            cnt[i][c-'a']++;
+        }
+    }
+    int res = 0;
+    pi best = {0, 0};
+    int n = in[0].size();
+    bool good = true;
+    for(int bcurr = 0; bcurr <= n && good; ++bcurr)
+    {
+        int ccurr = INFI;
+        for(int i = 0; i < 26; ++i)
+        {
+            if(cnt[0][i] - bcurr * cnt[1][i] < 0) {good = false; break;}
+            if(cnt[2][i] > 0) ccurr = min(ccurr, (cnt[0][i] - bcurr * cnt[1][i]) / cnt[2][i]);
+        }
+        if(!good) break;
+        if(bcurr + ccurr > res)
+        {
+            res = bcurr + ccurr;
+            best = {bcurr, ccurr};
+        }
+
+    }
+    for(int i = 0; i < 26; ++i)
+    {
+        cnt[0][i] -= cnt[1][i] * best.fi;
+        cnt[0][i] -= cnt[2][i] * best.se;
+    }
+    rep(i, best.fi)
+    {
+        cout << in[1];
+    }
+    rep(i, best.se)
+    {
+        cout << in[2];
+    }
+    rep(i, 26)
+    {
+        rep(j, cnt[0][i])
+        {
+            cout << (char)('a' + i);
+        }
+    }
+    cout <<'\n';
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+        cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
