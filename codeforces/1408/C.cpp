9b0881ce37b8cdaa51bfc2877d12ea2503665d5b
Lior Yehezkely <liorye03@gmail.com>
2020-09-30 18:01:00 +0300

    Add solution for problem `C - Discrete Acceleration` Link: https://codeforces.com/contest/1408/problem/C Tags: binary search, dp, implementation, math, two pointers, *1500 Ref: https://codeforces.com/contest/1408/submission/94311413

---
+++ codeforces/1408/C.cpp
@@ -0,0 +1,295 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+template<class T> using func = function<T>;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int maxn = 1e5 + 5;
+
+typedef long double ld;
+ll n, l;
+int a[maxn];
+#define BRUH_WHY_TESTCASES
+
+bool meet(ld t)
+{
+    ld pos = 0;
+    ld v = 1;
+    ld tot = 0;
+    bool bruh = true;
+    for(int i = 0; i < n; ++i)
+    {
+        if(tot + (a[i] - pos) / v > t)
+        {
+            pos += (t - tot) * v;
+            bruh = false; break;
+        }
+        tot += (a[i] - pos) / v;
+        pos = a[i];
+        v++;
+
+    }
+    if(bruh)
+    {
+        pos += (t - tot) * v;
+    }
+    ld pos2 = l;
+    ld v2 = 1;
+    ld tot2 = 0;
+    bool bruh2 = true;
+    for(int i = n-1; i >= 0; --i)
+    {
+        if(tot2 + (pos2 - a[i]) / v2 > t)
+        {
+            pos2 -= (t - tot2) * v2;
+            bruh2 = false; break;
+        }
+        tot2 += (pos2 - a[i]) / v2;
+        pos2 = a[i];
+        v2++;
+
+    }
+    if(bruh2)
+    {
+        pos2 -= (t - tot2) * v2;
+    }
+    return pos > pos2;
+
+}
+void solve()
+{
+    cin >> n >> l;
+    aread(a, n);
+    ld h = 1e10; ld low = 0;
+    while (h - low > (ld)1e-7)
+    {
+        ld m = low + (h - low) / 2.0;
+        if(meet(m))
+        {
+            h = m;
+        }
+        else
+        {
+            low = m;
+        }
+        
+
+    }
+    cout << low << '\n';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    cout << fixed << setprecision(8);
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+        cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
