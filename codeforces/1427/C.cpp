ccd35f24c6123cf012b87a6cdfb7d113b1d44cb6
Lior Yehezkely <liorye03@gmail.com>
2020-10-14 19:44:00 +0300

    Add solution for problem `C - The Hard Work of Paparazzi` Link: https://codeforces.com/contest/1427/problem/C Tags: dp, *2000 Ref: https://codeforces.com/contest/1427/submission/95526533

---
+++ codeforces/1427/C.cpp
@@ -0,0 +1,243 @@
+
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+ 
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+ 
+*/
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pi,
+null_type,
+less<pi>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ 
+ 
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+ 
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+ 
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+ 
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+ 
+
+
+template<class T> using func = function<T>;
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+ 
+ 
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+//#define BRUH_WHY_TESTCASES 
+
+const int maxn = 1e5 + 5;
+
+struct mission
+{
+    int x, y, t;
+} mis[maxn];
+int dp[maxn]; // dp[i] - the answer when I pick time i
+void solve()
+{
+    memset(dp, 0xFF, sizeof(dp)); // set all values initally to -1
+    int r, n; cin >> r >> n;
+    rep(i, n)
+    {
+        cin >> mis[i+1].t >> mis[i+1].x >> mis[i+1].y;
+    }
+    mis[0].x = 1;
+    mis[0].y = 1;
+    mis[0].t = 0;
+    dp[0] = 0;
+    int next = 0;
+    int mx = -1;
+    int ans = 0;
+    for(int i = 1; i <= n; ++i)
+    {
+        while(mis[i].t - mis[next].t >= 2 * r)
+        {
+            mx = max(mx, dp[next]);
+            next++;
+        }
+        if(mx >= 0)
+        {
+            dp[i] = mx + 1;
+        }
+        for(int j = i - 1; j >= 0 && mis[i].t - mis[j].t < 2 * r; --j)
+        {
+            if(abs(mis[j].x - mis[i].x) + abs(mis[j].y - mis[i].y) <= mis[i].t - mis[j].t && dp[j] >= 0)
+            {
+                dp[i] = max(dp[i], 1 + dp[j]);
+            }
+        }
+        ans = max(ans, dp[i]);
+    }
+    cout << ans << '\n';
+
+
+
+}
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+signed main()
+{
+    GOGOGO
+    cout << fixed << setprecision(6);
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+ 
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
