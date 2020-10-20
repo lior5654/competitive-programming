1ab4e594f11e22d6187fad73466f6825e73730f3
Lior Yehezkely <liorye03@gmail.com>
2020-10-20 17:21:00 +0300

    Add solution for problem `C - XOR Inverse` Link: https://codeforces.com/contest/1416/problem/C Tags: bitmasks, data structures, divide and conquer, dp, greedy, math, sortings, strings, trees, *2000 Ref: https://codeforces.com/contest/1416/submission/96081307

---
+++ codeforces/1416/C.cpp
@@ -0,0 +1,254 @@
+
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+ 
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+ 
+*/
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pi,
+null_type,
+less<pi>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ 
+ 
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+ 
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+ 
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+ 
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+ 
+
+
+template<class T> using func = function<T>;
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+ 
+ 
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+//#define BRUH_WHY_TESTCASES 
+const int maxn = 3e5 +5;
+int a[maxn];
+int n;
+int x = 0;
+ll res = 0;
+
+void solve()
+{
+    cin >> n;
+    aread(a, n);
+    vvi groups;
+    vi s;
+    rep(i, n)
+    {
+        s.pb(i);
+    }
+    groups.pb(s);
+    for(int b = 31; b >= 0; --b)
+    {
+        array<ll, 2> inv = { 0 };
+        for(auto g : groups)
+        {
+            array<ll, 2> cnt = { 0 };
+            for(auto i : g)
+            {
+                cnt[(a[i]&(1<<b))>0]++;
+                inv[(a[i]&(1<<b))>0] += cnt[!((a[i]&(1<<b))>0)];
+            } 
+        }
+        res += min(inv[0], inv[1]);
+        if(inv[0] > inv[1])
+        {
+            x |= 1<<b;
+            rep(i, n)
+            {
+                a[i] ^= 1<<b;
+            }
+        }
+        int sz = groups.size();
+        vvi next;
+        rep(i, sz)
+        {
+            array<vi, 2> split;
+            for(auto e : groups[i])
+            {
+                split[(a[e]&(1<<b))>0].pb(e);
+            }
+            for(auto s : split)
+            {
+                if(!s.empty())
+                {
+                    next.pb(s);
+                }
+            }
+        }
+        groups = next;
+    }
+    cout << res << ' ' << x << '\n';
+}
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+signed main()
+{
+    GOGOGO
+    cout << fixed << setprecision(6);
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+ 
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
