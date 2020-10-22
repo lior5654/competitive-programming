264d5e757ddf0f0286afa052ed2cd7cb3f34d20f
Lior Yehezkely <liorye03@gmail.com>
2020-10-22 18:40:00 +0300

    Add solution for problem `D - Imbalanced Array` Link: https://codeforces.com/contest/817/problem/D Tags: data structures, divide and conquer, dsu, sortings, *1900 Ref: https://codeforces.com/contest/817/submission/96379414

---
+++ codeforces/817/D.cpp
@@ -0,0 +1,243 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+ 
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+ 
+*/
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pi,
+null_type,
+less<pi>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ 
+ 
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+ 
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+ 
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+ 
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+ 
+ 
+ 
+template<class T> using func = function<T>;
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+ 
+ 
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+//#define BRUH_WHY_TESTCASES
+const int maxn  = 1e6 + 6;
+
+ll a[maxn];
+int lb[maxn]; int ls[maxn]; int rb[maxn]; int rs[maxn];
+int n;
+ll res = 0;
+void solve()
+{
+    cin >> n;
+    aread(a, n);
+    stack<int> s1; stack<int> s2;
+    rep(i, n)
+    {
+
+        while(!s1.empty() && a[s1.top()] < a[i]) s1.pop();
+        lb[i] = (s1.empty()) ? -1 : s1.top();
+        s1.push(i);
+        while(!s2.empty() && a[s2.top()] > a[i]) s2.pop();
+        ls[i] = (s2.empty()) ? -1 : s2.top();
+        s2.push(i);
+    }
+    while(!s1.empty()) s1.pop();
+    while(!s2.empty()) s2.pop();
+    for(int i = n - 1; i >= 0; --i)
+    {
+        while(!s1.empty() && a[s1.top()] <= a[i]) s1.pop();
+        rb[i] = (s1.empty()) ? n : s1.top();
+        s1.push(i);
+        while(!s2.empty() && a[s2.top()] >= a[i]) s2.pop();
+        rs[i] = (s2.empty()) ? n : s2.top();
+        s2.push(i);
+    }
+    rep(i, n)
+    { // [] (]
+        res += ((ll)(i - lb[i]) * (rb[i] - i) - (ll)(i - ls[i]) * (rs[i] - i)) * a[i];
+    }
+    cout << res << '\n';
+
+
+
+
+
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /*
+
+
+
+ # s.t bits are not all contained in the maximu. i.e there exists a bit b such that max[b] = 0 && exists l <= k <= r such that a[k][b] = 1
+ 
+ do segment tree over or + bs
+ */
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+signed main()
+{
+    GOGOGO
+    cout << fixed << setprecision(6);
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+ 
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
