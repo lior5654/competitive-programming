6da2d7c7c1c303b5aeb068686734d33be3fca835
Lior Yehezkely <liorye03@gmail.com>
2020-10-22 09:26:00 +0300

    Add solution for problem `F - Binary String Reconstruction` Link: https://codeforces.com/contest/1352/problem/F Tags: constructive algorithms, dfs and similar, math, *1500 Ref: https://codeforces.com/contest/1352/submission/96333229

---
+++ codeforces/1352/F.cpp
@@ -0,0 +1,237 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+ 
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+ 
+*/
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pi,
+null_type,
+less<pi>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ 
+ 
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+ 
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+ 
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+ 
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+ 
+ 
+ 
+template<class T> using func = function<T>;
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+ 
+ 
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+#define BRUH_WHY_TESTCASES
+
+
+void solve()
+{
+    int n0, n1, n2; cin >> n0 >> n1 >> n2;
+    // the number of swaps I have should be n1
+    if(n1 == 0)
+    {
+        if(n0) {
+            rep(i, n0 + 1)
+            {
+                cout << '0';
+            }
+        }
+        else
+        {
+            rep(i, n2 + 1) {
+                cout << '1';
+            }
+        }
+        cout << '\n'; return;
+        
+    }
+    rep(i, n0 + 1)
+    {
+        cout << '0';
+    }
+    rep(i, n2 + 1) {
+        cout << '1';
+    }
+    bool f = false;
+    rep(i, n1 - 1) {
+        cout << (f ? '1' : '0');
+        f = !f;
+    }
+    cout << '\n'; return;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /*
+
+
+
+ # s.t bits are not all contained in the maximu. i.e there exists a bit b such that max[b] = 0 && exists l <= k <= r such that a[k][b] = 1
+ 
+ do segment tree over or + bs
+ */
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+signed main()
+{
+    GOGOGO
+    cout << fixed << setprecision(6);
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+ 
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
