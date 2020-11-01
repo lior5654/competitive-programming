7cbadbfd5a731578e00caa89e890fb889da80b8c
Lior Yehezkely <liorye03@gmail.com>
2020-11-01 13:30:00 +0200

    Add solution for problem `A - Division` Link: https://codeforces.com/contest/1444/problem/A Tags: brute force, math, number theory, *1500 Ref: https://codeforces.com/contest/1444/submission/97318622

---
+++ codeforces/1444/A.cpp
@@ -0,0 +1,295 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+template<class T> using func = function<T>;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int maxn = 0;
+
+// be calm and focused
+
+#define BRUH_WHY_TESTCASES
+
+int n;
+
+vpl primeFactors(ll k)  
+{  
+    // Print the number of 2s that divide n  
+    vpl out;
+    ll meme = 1;
+    while (k % 2ll == 0)  
+    {  
+        meme *= 2ll;
+        k = k/2ll;  
+    }  
+    if(meme > 1)
+    {
+        out.emplace_back(2ll, meme);
+    }
+  
+    // n must be odd at this point. So we can skip  
+    // one element (Note i = i +2)  
+    for (ll i = 3; i <= sqrt(k); i = i + 2ll)  
+    {  
+        // While i divides n, print i and divide n  
+        meme = 1;
+        while (k % i == 0ll)  
+        {  
+            meme *= i;
+            k = k/i;  
+        }  
+        if(meme > 1)
+        {
+            out.emplace_back(i, meme);
+        }
+    }  
+  
+    // This condition is to handle the case when n  
+    // is a prime number greater than 2  
+    if (k > 2ll)  
+        out.pb({k, k});
+    return out;
+}  
+  
+
+void solve()
+{
+    ll p, q; cin >> p >> q;
+    if(p % q)
+    {
+        cout << p << '\n'; return;
+    }
+    vpl primes = primeFactors(q);
+    ll bres = INFL;
+    int sz = primes.size();
+    for(int i = 0; i < sz; ++i)
+    {
+        ll tmp = p;
+        ll res = 1;
+        while(!(tmp%primes[i].fi) && tmp >= primes[i].se && tmp >= q && !(tmp%q))
+        {
+            tmp /= primes[i].fi;
+            res *= primes[i].fi;
+        }
+        if(res < bres)
+        {
+            bres = res;
+        }
+    }
+
+    cout << p / bres  << '\n'; return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+        cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
