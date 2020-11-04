e5739201540caad9ba35c0f53496fb616ab4f102
Lior Yehezkely <liorye03@gmail.com>
2020-11-04 14:22:00 +0200

    Add solution for problem `E - Long Permutation` Link: https://codeforces.com/contest/1443/problem/E Tags: brute force, math, two pointers, *2400 Ref: https://codeforces.com/contest/1443/submission/97631877

---
+++ codeforces/1443/E.cpp
@@ -0,0 +1,283 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+template<class T> using func = function<T>;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int maxn = 2e5 + 5;
+const ll manual = 15;
+ll n, q;
+ll fact[manual + 1];
+
+vl qperm(ll idx)
+{
+    assert(idx < fact[min(manual, n)]);
+    bitset<manual> left;
+    left.set();
+    vl out;
+    for(int i = manual - 1; i >= 0; --i)
+    {
+        ll lidx = idx / fact[i];
+        ll res = left._Find_first();
+        rep(j, lidx)
+        {
+            res = left._Find_next(res);
+        }
+        out.pb(res+ 1 + n - manual);
+        left[res] = 0;
+        idx = idx % fact[i];
+
+    }
+    return out;
+
+
+}
+void init()
+{
+    fact[0] = 1;
+    fact[1] = 1;
+    for(ll i = 2; i <= manual; ++i)
+        fact[i] = i * fact[i-1];
+}
+void solve()
+{
+    init();
+    cin >> n >> q;
+    ll cx = 0;
+    rep(i, q) {
+        ll t; cin >> t;
+        if(t == 1) {
+            ll l, r; cin >> l >> r; --l; --r;
+            ll res = 0;
+            // [l, r]
+            // [0, n - 16]
+            // [n - 15, n - 1]
+            if(l <= n - manual - 1) {
+                pl ins = {l, min(n  - manual - 1, r)};
+                res += ((ins.se + 1ll) * (ins.se + 2ll)) / 2;
+                res -= ((ins.fi) * (ins.fi + 1ll)) / 2;
+            }
+            if(r >= n-manual) {
+                vl perm = qperm(cx);
+                for(int idx = max(l, n - manual); idx <= r; ++idx)
+                {
+                    res += perm[manual - 1 - (n - 1 - idx)];
+                }
+            }
+            cout << res << '\n';
+        }
+        else {
+            ll x; cin >> x;
+            cx += x;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+        cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
