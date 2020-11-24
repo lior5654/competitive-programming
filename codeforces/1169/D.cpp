726821165e61965910b2b02d3c3c50bac1775273
Lior Yehezkely <liorye03@gmail.com>
2020-11-24 14:23:00 +0200

    Add solution for problem `D - Good Triple` Link: https://codeforces.com/contest/1169/problem/D Tags: brute force, *1900 Ref: https://codeforces.com/contest/1169/submission/99393323

---
+++ codeforces/1169/D.cpp
@@ -0,0 +1,249 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+ 
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+ 
+*/
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pi,
+null_type,
+less<pi>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ 
+ 
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+ 
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+ 
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+ 
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+ 
+ 
+ 
+template<class T> using func = function<T>;
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+const ll mod = 998244353;
+template<int maxf> struct combi
+{
+    ll fact[maxf];
+    ll invfact[maxf];
+    ll bexp(ll b, ll p) const
+    {
+        ll res = 1;
+        ll curr = b;   
+        while(p)
+        {
+            if(p % 2ll)    
+            {
+                res *= curr;
+                res %= mod;
+            }
+
+            curr *= curr;
+            curr %= mod;
+            p /= 2;
+        }
+        return res;
+    }
+    ll invmod(ll i) const
+    {
+        return bexp(i, mod - 2ll);
+    }
+    combi()
+    {
+        fact[0] = 1;
+        fact[1] = 1;
+        invfact[0] = invmod(1);
+        invfact[1] = invfact[0];
+        for(ll i = 2; i < maxf; ++i)
+        {
+            fact[i] = i * fact[i-1];
+            fact[i] %= mod;
+            invfact[i] = invmod(i);
+        }
+    }
+    ll choose(ll n, ll k) const
+    {
+        if(k > n) return 0;
+        return (((fact[n] * invfact[n-k]) % mod) * invfact[k]) % mod;
+    }
+};
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+//#define BRUH_WHY_TESTCASES
+
+const int maxn = 3e5 + 5;
+
+
+string s;
+void solve()
+{
+    cin >> s;
+    const int n = s.size();
+    ll res = 0;
+    int old = -1;
+    for(int i = 2; i < n; ++i)
+    {
+        int found = 1;
+        bool good = false;
+        for(found = 1; i - 2 * found >= max(0, old); ++found)
+        {
+            if(s[i] == s[i-found] && s[i-found] == s[i-2*found]) {good = true; break;}
+        }
+
+        if(good)
+        {
+            res += i - 2 * found + 1;
+            old= i - 2*found;
+        }
+        else if (old >= 0)
+        {
+            res += old + 1;
+        }
+        
+    }
+    cout << res << '\n';
+
+
+}
+ 
+ 
+ 
+ 
+signed main()
+{
+    GOGOGO
+    cout << fixed << setprecision(6);
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+ 
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
