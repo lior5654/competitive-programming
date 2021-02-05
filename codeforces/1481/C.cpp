ca33ec96763d16284185e0725a6156a1964604cf
Lior Yehezkely <liorye03@gmail.com>
2021-02-05 17:10:00 +0200

    Add solution for problem `C - Fence Painting` Link: https://codeforces.com/contest/1481/problem/C Tags: brute force, constructive algorithms, greedy, *1600 Ref: https://codeforces.com/contest/1481/submission/106564950

---
+++ codeforces/1481/C.cpp
@@ -0,0 +1,275 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+template<class T> using func = function<T>;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int maxn = 1e5 + 5;
+int a[maxn]; int b[maxn]; int c[maxn]; int n; int m;
+int ans[maxn];
+stack<int> g[maxn];
+int vis[maxn] = {0};
+#define BRUH_WHY_TESTCASES
+void solve()
+{
+    cin >> n >> m;
+    for(int i = 1; i <= n; ++i) {
+        vis[i] = -1;
+        g[i] = stack<int>();
+    }
+    rep(i, n) {
+        cin >> a[i+1];
+    }
+    rep(i, n) {
+        cin >> b[i+1];
+        if(a[i+1] != b[i+1]) {
+            g[b[i+1]].push(i+1);
+        } 
+        vis[b[i+1]] = i+1;
+    }
+    rep(i, m) {
+        cin >> c[i+1];
+    }
+
+    for(int i = m; i >= 1; --i) {
+        if(!g[c[i]].empty()) {
+            int to = g[c[i]].top(); g[c[i]].pop();
+            ans[i] = to;
+        } else {
+            if(i!= m) {
+                ans[i] = ans[i+1];
+            } else {
+                if(vis[c[i]]>=0) {
+                    ans[i] = vis[c[i]];
+                } else {
+                    cout << "NO" << '\n'; return;
+                }
+            }
+        }
+    }
+    for(int i = 1; i <= n; ++i) {
+        if(!g[i].empty()) {
+            cout << "NO" << '\n'; return;
+        }
+    }
+    cout << "YES" << '\n';
+    for(int i = 1; i <= m; ++i) {
+        cout << ans[i] << ' ';
+    }
+    cout << '\n';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+        cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
