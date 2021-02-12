d62754e73c80b5f729d0b7565c335af504e15d30
Lior Yehezkely <liorye03@gmail.com>
2021-02-12 17:13:00 +0200

    Add solution for problem `B - Replace and Keep Sorted` Link: https://codeforces.com/contest/1485/problem/B Tags: dp, implementation, math, *1200 Ref: https://codeforces.com/contest/1485/submission/107200039

---
+++ codeforces/1485/B.cpp
@@ -0,0 +1,264 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/ `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+template<class T> using func = function<T>;
+
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 4e18 + 5;
+
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+
+
+// LIORZ LIORZ LIORZ LIORZ LIORZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int maxn = 1e5 + 5;
+ll a[maxn];
+ll v[maxn];
+ll pre[maxn] = { 0 };
+int n, q, k;
+//#define BRUH_WHY_TESTCASES
+void solve()
+{
+    cin >> n >> q >> k;
+    rep(i, n) {
+        cin >> a[i];
+    }
+    if(n == 1) {
+        v[0] = k-1;
+    } else {
+        v[0] = max(0LL, a[1] - 1 - 1);
+        v[n-1] = max(0LL, k - a[n-2] - 1);
+    }
+    for(int i = 1; i < n - 1; ++i) {
+        v[i] = max(0LL, a[i+1] - a[i-1] - 2);
+    }
+    for(int i = 0; i < n; ++i) {
+        pre[i] = v[i] + (i==0?0:pre[i-1]);
+    }
+    /*
+    rep(i, n) {
+        cout << v[i] << ' ';
+    }
+    cout << '\n';*/
+    rep(i, q) {
+        int l, r; cin >> l >> r; --l; --r;
+        if(l == r) {
+            cout << k - 1 << '\n';
+        } else {
+            ll res = max(0LL, a[l+1] - 2);
+            res += max(0LL, k - a[r-1] - 1);
+            l++;
+            r--;
+            if(l <= r) {
+                res += pre[r] - (l==0?0:pre[l-1]);
+            }
+            cout << res << '\n';
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    GOGOGO
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+        cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
