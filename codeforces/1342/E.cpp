fef0b9b9ce9c3a305969ac3a395947a89b45c116
Lior Yehezkely <liorye03@gmail.com>
2021-03-20 01:32:00 +0200

    Add solution for problem `E - Placing Rooks` Link: https://codeforces.com/contest/1342/problem/E Tags: combinatorics, fft, math, *2300 Ref: https://codeforces.com/contest/1342/submission/110490430

---
+++ codeforces/1342/E.cpp
@@ -0,0 +1,237 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+ 
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+ 
+*/
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pl,
+null_type,
+less<pl>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ 
+ 
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+ 
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+ 
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+ 
+const int INFI = 1e9 + 5;
+const ll INFL = 1e17 + 5;
+ 
+ 
+ 
+template<class T> using func = function<T>;
+template<class T> void aread(T* arr, int ___n)
+{
+    rep(i, ___n)
+    {
+        cin >> arr[i];
+    }
+}
+ 
+ 
+ 
+ 
+ 
+//#define BRUH_WHY_TESTCASES
+//#define PREC 10
+//#define USACO "sleepy"
+
+const int maxn = 2e5 + 5;
+const ll mod = 998244353;
+const int maxf = 2e5 + 5;
+
+ll fact[maxn];
+ll ifact[maxn];
+int n; ll k;
+
+ll mmul(ll l, ll r) {
+    return (l*r)%mod;
+}
+ll bexp(ll b, ll p) {
+    ll res = 1; ll curr = b;
+    while(p) {
+        if(p%2) {
+            res *= curr; res %= mod;
+        }
+        p /= 2;
+        curr *= curr; curr %= mod;
+    }
+    return res;
+}
+ll inv(ll x) {
+    return bexp(x, mod-2);
+}
+void init_comb() {
+    fact[0] = 1; fact[1] = 1; 
+    for(ll i = 2; i < maxf; ++i) {
+        fact[i] = mmul(i,fact[i-1]);
+    }
+    for(ll i = 0; i < maxf; ++i) {
+        ifact[i] = inv(fact[i]);
+    }
+}
+ll choose(ll x, ll y) {
+    if(y>x) return 0;
+    return mmul(mmul(fact[x],ifact[y]),ifact[x-y]);
+}
+void solve() {
+    init_comb();
+    cin >> n >> k;
+    if(k > n) {
+        cout << 0 << '\n'; return;
+    }
+    if(k == 0) {
+        cout << fact[n] << '\n'; return;
+    }
+    ll res = bexp(n-k, n);
+    ll invr = 0;
+    for(ll i = 1, cmult = 1; i <= n-k-1; ++i, cmult *= -1) {
+        invr += mmul(cmult,mmul(choose(n-k, i),bexp(n-k-i,n)));
+        invr %= mod;
+    }
+    res -= invr;
+    res = ((res%mod)+mod)%mod;
+    
+    
+    cout << (res * choose(n, n-k) * 2LL) % mod << '\n';
+
+
+}
+ 
+ 
+ 
+ 
+signed main()
+{
+    #ifdef USACO
+        freopen(USACO ".in", "r", stdin);
+        freopen(USACO ".out", "w", stdout);
+    #endif
+    GOGOGO
+    #ifdef PREC
+      cout << fixed << setprecision(PREC);
+    #endif
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+ 
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
