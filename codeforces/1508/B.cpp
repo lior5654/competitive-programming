d88cf0a46cb922b5ecc6a43cef9e70fefe54d2fe
Lior Yehezkely <liorye03@gmail.com>
2021-04-16 18:29:00 +0300

    Add solution for problem `B - Almost Sorted` Link: https://codeforces.com/contest/1508/problem/B Tags: binary search, combinatorics, constructive algorithms, implementation, *1800 Ref: https://codeforces.com/contest/1508/submission/113224286

---
+++ codeforces/1508/B.cpp
@@ -0,0 +1,256 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+ 
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+ 
+*/
+ 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including 
+ 
+using namespace __gnu_pbds;
+using namespace std;
+ 
+ 
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pl,
+null_type,
+less<pl>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+ 
+ 
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+ 
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+ 
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+ 
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+ 
+const int INFI = 1e9 + 5;
+const ll INFL = 1e17 + 5;
+ 
+ 
+ 
+template<class T> using func = function<T>;
+template<class T> void aread(T* arr, int _n)
+{
+    rep(i, _n)
+    {
+        cin >> arr[i];
+    }
+}
+ 
+ 
+ 
+ 
+ 
+#define BRUH_WHY_TESTCASES
+//#define PREC 10
+//#define USACO "sleepy"
+
+ll n, k;
+const int UPPER = 61;
+ll dp[100][100];
+ll sdp[100];
+void solve() {
+    cin >> n >> k;
+    int next = 1;
+    vi res;
+    while(n > UPPER) {
+        res.pb(next);
+        next++;
+        n--;
+    }
+    vi left;
+    rep(i, n) {
+        left.pb(next+i);
+    }
+    ll mc = 0;
+    int i = 0;
+    while(i < n) {
+        int j = 1;
+        int nn = (n-i);
+        while(j < nn && mc + dp[j][nn] < k) {
+            mc += dp[j][nn];
+            j++;
+        }
+        for(int e = j-1; e >= 0; --e) {
+            res.pb(left[e]);
+        }
+        vi nleft;
+        for(int e = j; e < left.size(); ++e) {
+            nleft.pb(left[e]);
+        }
+        left=nleft;
+        i += j;
+    }
+    if(mc != k-1) {
+        cout << -1 << '\n'; return;
+    }
+    for(auto e : res) {
+        cout << e << ' ';
+    }
+    cout << '\n';
+
+
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ void init() {
+    dp[1][0] = 1;
+    dp[1][1] = 1;
+    sdp[0] = 1;
+    sdp[1] = 1;
+    for(int i = 2; i <= UPPER; ++i) {
+        for(int j = 1; j <= i; ++j) {
+            dp[j][i] = sdp[i-j];
+            sdp[i] += dp[j][i];
+        }
+    }
+ }
+ 
+ 
+ 
+signed main()
+{
+    #ifdef USACO
+        freopen(USACO ".in", "r", stdin);
+        freopen(USACO ".out", "w", stdout);
+    #endif
+    GOGOGO
+    
+    #ifdef PREC
+      cout << fixed << setprecision(PREC);
+    #endif
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    init();
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+ 
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
