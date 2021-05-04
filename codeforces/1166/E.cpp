3a861b291d770e34044e2ec2c0374c4a17717e99
Lior Yehezkely <liorye03@gmail.com>
2021-05-05 00:19:00 +0300

    Add solution for problem `E - The LCMs Must be Large` Link: https://codeforces.com/contest/1166/problem/E Tags: bitmasks, brute force, constructive algorithms, math, number theory, *2100 Ref: https://codeforces.com/contest/1166/submission/115151189

---
+++ codeforces/1166/E.cpp
@@ -0,0 +1,265 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+//typedef __int128_t bigint;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pl,
+null_type,
+less<pl>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 1e18 + 5;
+
+template<class T> using func = function<T>;
+
+/************************************ general.hpp begin ************************************/
+
+namespace CompetitiveProgramming
+{
+
+using bigint = long long int;
+using ll = long long int;
+using ftype = long double;
+using ld = long double;
+
+namespace IO
+{
+
+template<class ElementType> inline void output_array(ElementType* array, uint32_t size)
+{
+    if(size)
+    {
+        cout << *array++;
+        size--;
+        while(size)
+        {
+            cout << ' ' << *array++;
+            --size;
+        }
+    }
+    cout << '\n';
+}
+
+template<class Iterator> inline void output_iterator_range(Iterator begin, const Iterator& end)
+{
+    if(begin != end)
+    {
+        cout << *begin++;
+        while(begin != end)
+        {
+            cout << ' ' << *begin++;
+        }
+    }
+    cout << '\n';
+}
+
+template<class Container> inline void output_container(const Container& container)
+{
+    output_iterator_range(container.begin(), container.end());
+}
+
+template<class ElementType> inline void read_array(ElementType* array, uint32_t size)
+{
+    while(size)
+    {
+        cin >> *array++;
+        --size;
+    }
+}
+
+template<class Iterator> inline void read_iterator_range(Iterator begin, const Iterator& end)
+{
+    while(begin != end)
+    {
+        cin >> *begin++;
+    }
+}
+template<class Container> inline void read_container(Container& container)  // I dont think this will work for set etc, only std::array, std::vector etc
+{
+    read_iterator_range(container.begin(), container.end());
+}
+
+}
+
+}
+
+
+using namespace CompetitiveProgramming;
+using namespace IO;
+
+/************************************ general.hpp end ************************************/
+
+
+
+//#define BRUH_WHY_TESTCASES
+//#define PREC 10
+//#define USACO "sleepy"
+
+const int maxn = 1e4 + 5;
+const int maxm = 55;
+bitset<maxn> st[maxm];
+void solve() {
+    int n, m; cin >> m >> n;
+    rep(i, m) {
+        int s; cin >> s;
+        rep(j, s) {
+            int x; cin >> x;
+            st[i][x] = 1;
+        }
+    }
+
+    for(int i = 0; i < m; ++i) {
+        for(int j = i+1; j < m; ++j) {
+            if((st[i]&st[j]).count() == 0) {
+                cout << "impossible" << '\n'; return;
+            }
+        }
+    }
+    cout << "possible" << '\n';
+
+}
+
+
+
+
+signed main()
+{
+    #ifdef USACO
+        freopen(USACO ".in", "r", stdin);
+        freopen(USACO ".out", "w", stdout);
+    #endif
+    GOGOGO
+    #ifdef PREC
+      cout << fixed << setprecision(PREC);
+    #endif
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
+
+
+
+
+
+
+
+
+
+
+
