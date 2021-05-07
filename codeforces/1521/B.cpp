e847be605c825d0a255150a79195430cb5434fbe
Lior Yehezkely <liorye03@gmail.com>
2021-05-07 18:12:00 +0300

    Add solution for problem `B - Nastia and a Good Array` Link: https://codeforces.com/contest/1521/problem/B Tags: constructive algorithms, math, number theory, *1300 Ref: https://codeforces.com/contest/1521/submission/115563171

---
+++ codeforces/1521/B.cpp
@@ -0,0 +1,274 @@
+/*
+                            . .  ,  ,
+                            |` \/ \/ \,',
+                            ;          ` \/\,.
+                           :               ` \,/
+                           |                  /
+                           ;                 :
+                          :                  ;
+                          |      ,---.      /
+                         :     ,'     `,-._ \
+                         ;    (   o    \   `'
+                       _:      .      ,'  o ;
+                      /,.`      `.__,'`-.__,
+                      \_  _               \
+                     ,'  / `,          `.,'
+               ___,'`-._ \_/     `,._        ;
+            __;_,'      `-.`-'./ `--.____)
+         ,-'           _,--\^-'
+       ,:_____      ,-'     \
+      (,'     `--.  \;-._    ;
+      :    Y      `-/    `,  :
+      :    :       :     /_;'
+      :    :       |    :
+       \    \      :    :
+        `-._ `-.__, \    `.
+           \   \  `. \     `.
+         ,-;    \---)_\ ,','/
+         \_ `---'--'" ,'^-;'
+         (_`     ---'" ,-')
+         / `--.__,. ,-'    \
+-hrr-    )-.__,-- ||___,--' `-.
+        /._______,|__________,'\
+        `--.____,'|_________,-'
+
+                             __
+                   _ ,___,-'",-=-.
+       __,-- _ _,-'_)_  (""`'-._\ `.
+    _,'  __ |,' ,-' __)  ,-     /. |
+  ,'_,--'   |     -'  _)/         `\
+,','      ,'       ,-'_,`           :
+,'     ,-'       ,(,-(              :
+     ,'       ,-' ,    _            ;
+    /        ,-._/`---'            /
+   /        (____)(----. )       ,'
+  /         (      `.__,     /\ /,
+ :           ;-.___         /__\/|
+ |         ,'      `--.      -,\ |
+ :        /            \    .__/
+  \      (__            \    |_
+   \       ,`-, *       /   _|,\
+    \    ,'   `-.     ,'_,-'    \
+   (_\,-'    ,'\")--,'-'       __\
+    \       /  // ,'|      ,--'  `-.
+     `-.    `-/ \'  |   _,'         `.
+        `-._ /      `--'/             \
+-hrr-      ,'           |              \
+          /             |               \
+       ,-'              |               /
+      /                 |             -'
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+typedef long long int ll;
+//typedef __int128_t bigint;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef tree<
+pl,
+null_type,
+less<pl>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+
+typedef long double ld;
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define repk(i, k, n) for(int i = k; i < n; ++i)
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define gcd std::__detail::__gcd
+#define lcm std::__detail::__lcm
+
+const int INFI = 1e9 + 5;
+const ll INFL = 1e18 + 5;
+
+template<class T> using func = function<T>;
+
+/************************************ general.hpp begin ************************************/
+
+namespace CompetitiveProgramming
+{
+
+using bigint = long long int;
+using ll = long long int;
+using ftype = long double;
+using ld = long double;
+
+namespace IO
+{
+
+template<class ElementType> inline void output_array(ElementType* array, uint32_t size)
+{
+    if(size)
+    {
+        cout << *array++;
+        size--;
+        while(size)
+        {
+            cout << ' ' << *array++;
+            --size;
+        }
+    }
+    cout << '\n';
+}
+
+template<class Iterator> inline void output_iterator_range(Iterator begin, const Iterator& end)
+{
+    if(begin != end)
+    {
+        cout << *begin++;
+        while(begin != end)
+        {
+            cout << ' ' << *begin++;
+        }
+    }
+    cout << '\n';
+}
+
+template<class Container> inline void output_container(const Container& container)
+{
+    output_iterator_range(container.begin(), container.end());
+}
+
+template<class ElementType> inline void read_array(ElementType* array, uint32_t size)
+{
+    while(size)
+    {
+        cin >> *array++;
+        --size;
+    }
+}
+
+template<class Iterator> inline void read_iterator_range(Iterator begin, const Iterator& end)
+{
+    while(begin != end)
+    {
+        cin >> *begin++;
+    }
+}
+template<class Container> inline void read_container(Container& container)  // I dont think this will work for set etc, only std::array, std::vector etc
+{
+    read_iterator_range(container.begin(), container.end());
+}
+
+}
+
+}
+
+
+using namespace CompetitiveProgramming;
+using namespace IO;
+
+/************************************ general.hpp end ************************************/
+
+
+
+#define BRUH_WHY_TESTCASES
+//#define PREC 10
+//#define USACO "sleepy"
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+auto DIST = uniform_int_distribution<ll>(0, INFL);
+
+ll grnd()
+{
+    return DIST(rng);
+}
+
+
+const int maxn = 2e5 +5;
+
+int a[maxn];
+int n;
+void solve() {
+    cin >> n;
+    int pos = 0;
+    rep(i, n) {
+        cin >> a[i];
+        if(a[i] < a[pos]) {
+            pos = i;
+        }
+    }
+
+
+    vector<pair<pi, pi>> res;
+    if(pos != 0) {
+         res.pb({{1, pos+1}, {a[pos], a[0]}});
+        swap(a[0], a[pos]);
+    }
+    rep(i, n-1) {
+        if(a[i] <= a[i+1]) {
+            res.pb({{i+1, i+2}, {a[i], a[i]+1}});
+            a[i+1] = a[i] + 1;
+        } else {
+            res.pb({{1, i+2}, {a[0], a[i]-1}});
+            a[i+1] = a[i] - 1;
+        }
+    }
+    cout << res.size() << '\n';
+    for(auto e : res) {
+        cout << e.fi.fi << ' ' << e.fi.se << ' ' << e.se.fi << ' ' << e.se.se << '\n';
+    }
+}
+
+
+
+
+signed main()
+{
+    #ifdef USACO
+        freopen(USACO ".in", "r", stdin);
+        freopen(USACO ".out", "w", stdout);
+    #endif
+    GOGOGO
+    #ifdef PREC
+      cout << fixed << setprecision(PREC);
+    #endif
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+    while(t--)
+    {
+        solve();
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
