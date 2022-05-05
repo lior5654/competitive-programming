dcb095bb68e69dd08e312ffb4e196ecbb031f6b7
Lior Yehezkely <liorye03@gmail.com>
2022-05-05 18:38:00 +0300

    Add solution for problem `F - Vlad and Unfinished Business` Link: https://codeforces.com/contest/1675/problem/F Tags: dfs and similar, dp, greedy, trees, *1800 Ref: https://codeforces.com/contest/1675/submission/155976216

---
+++ codeforces/1675/F.cpp
@@ -0,0 +1,240 @@
+
+/****************************** begin /template.h ******************************/
+
+/***********************************************************************
+ * [?] APIO 2021 Template
+ * [+] Made By: Lior Yehezkely, FiveSixFiveFour
+ * [~] Apples are ORZ
+ * *********************************************************************/
+
+/*** begin #define flags ***/
+// #define USE_ORDERED_STATISTICS
+// #define BIG_BIGINT
+/*** end #define flags ***/
+
+/*** begin includes ***/
+#include <bits/stdc++.h>
+
+#ifdef USE_ORDERED_STATISTICS
+    #include <ext/pb_ds/assoc_container.hpp>
+    #include <ext/pb_ds/tree_policy.hpp>
+    using namespace __gnu_pbds;
+#endif
+
+using namespace std;
+/*** end includes ***/
+
+/*** begin typedefs ***/
+typedef long long int ll;
+#ifdef BIG_BIGINT
+    typedef __int128_t bigint;
+#else
+    typedef ll bigint;
+#endif
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vpi> vvpi;
+typedef vector<vpl> vvpl;
+typedef set<int> si;
+typedef multiset<int> msi;
+typedef set<ll> sl;
+typedef multiset<ll> msl;
+typedef long double ld;
+template<class T> using func = function<T>;
+
+#ifdef USE_ORDERED_STATISTICS
+    typedef tree<
+    pl,
+    null_type,
+    less<pl>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>
+    ordered_set;
+#endif
+
+/*** end typedefs ***/
+
+/*** begin #defines ***/
+#define clrcin cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define GOGOGO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define BYEBYE return 0;
+
+#define all(cn) (cn).begin(), (cn).end()
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define csz(c) ((int)c.size())
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+#define popcnt __builtin_popcount
+#define popcntll __builtin_popcount_ll
+
+/*** end #defines ***/
+
+const int INFI = 1e9 + 5;
+const ll INFL = 1e18 + 5;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); 
+auto dist = uniform_int_distribution<int>(0, INFI); 
+auto distll = uniform_int_distribution<ll>(0, INFL); 
+int rnd() { return dist(rng); }
+ll rndl() { return distll(rng); }
+
+/****************************** end /template.h ******************************/
+
+
+/****************************** begin /main.cpp ******************************/
+
+
+/*** begin #define flags ***/
+#define BRUH_WHY_TESTCASES
+//#define PREC 7
+//#define INPUT_FILE "hi.in"
+//#define OUTPUT_FILE "hi.out"
+/*** end #define flags ***/
+
+const int maxn= 2e5 + 5;
+
+int n; int x, y; int k;
+
+vi g[maxn];
+int d[maxn];
+bitset<maxn> things = 0;
+//bitset<maxn> needed = 0;
+ll res = 0;
+bool dfs(int c, int p = -1, int dd = 0) {
+    d[c] = dd;
+    bool found = things[c];
+    for(auto e : g[c]) {
+        if(e != p) {
+            if(dfs(e, c, dd+1)) {
+                found = true; res += 2;
+            }
+        }
+    }
+    //needed[c] = found;
+
+    return found;
+}
+
+void clean() {
+    for(int i = 1; i <= n; i++) {
+        d[i] = 0; g[i] = vi();
+    }
+    res = 0; n = 0; x = 0; y = 0;
+    things = 0;
+}
+void solve(int t) {
+    cin >> n >> k;
+    cin >> x >> y;
+
+    rep(i, k) {
+        int lol; cin >> lol;
+        things[lol] = true;
+    }
+    things[y] = true;
+
+    for(int i = 1; i <= n-1; i++) {
+        int u, v; cin >> u >> v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+
+    dfs(x);
+
+    
+
+    cout << res - d[y] << '\n';
+    clean();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+signed main()
+{
+    #ifdef INPUT_FILE
+        freopen(INPUT_FILE, "r", stdin);
+    #endif
+    #ifdef OUTPUT_FILE
+        freopen(OUTPUT_FILE, "w", stdout);
+    #endif
+    GOGOGO
+    #ifdef PREC
+      cout << fixed << setprecision(PREC);
+    #endif
+    int t=1;
+    #ifdef BRUH_WHY_TESTCASES
+            cin >> t;
+    #endif
+
+    int count = 1;
+    while(t--)
+    {
+        //cout << "Case #" << count << ":";
+        solve(count);
+        count += 1;
+    }
+    BYEBYE
+}
+
+/* PLEASE READ THIS
+* N = 1
+* GUESS A!!!!!!!
+* DO SOMETHING INSTEAD OF NOTHING
+*/
+
+/****************************** end /main.cpp ******************************/
+
+
+
+
+
